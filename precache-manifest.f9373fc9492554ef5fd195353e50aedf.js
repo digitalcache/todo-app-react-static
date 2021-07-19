f8358ce0789bc0d1cb944894f29b2420a8415f2c
dcharlis <dcharlis@pkglobal.com>
2021-07-19 22:16:55 +0530

    Adding files

---
+++ precache-manifest.f9373fc9492554ef5fd195353e50aedf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6e7267aad34aeeb8c67c036b954417",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acefcb9c79bfc351ca00",
+    "url": "./static/css/main.10da8476.chunk.css"
+  },
+  {
+    "revision": "e2b123d6aec247e399e9",
+    "url": "./static/js/2.3e2a5d19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3e2a5d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acefcb9c79bfc351ca00",
+    "url": "./static/js/main.e16b065d.chunk.js"
+  },
+  {
+    "revision": "f2886ae4f32e95d0a917",
+    "url": "./static/js/runtime-main.b4c0ae84.js"
+  },
+  {
+    "revision": "08126b264f92ce72aa4d11977ec2a37c",
+    "url": "./static/media/add-blue.08126b26.svg"
+  },
+  {
+    "revision": "0e227ab5d4d12d19e92afa22b6abfdf6",
+    "url": "./static/media/checked.0e227ab5.svg"
+  },
+  {
+    "revision": "558bca77767174a04af182ea44a42a23",
+    "url": "./static/media/not-checked.558bca77.svg"
+  }
+]);
